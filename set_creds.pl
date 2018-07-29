613e758fd1757bb2000a20512521cfc3e06cef31
Itai Weisman <iweisman@infinidat.com>
2018-07-29 16:24:15 +0300

    modified:   set_creds.pl

--- set_creds.pl
+++ set_creds.pl
@@ -1,3 +1,21 @@
+# INFINIDAT Snapshot Handling Utility for HPUX
+## Written By Itai Weisman, Solution Engineering Team Leader, INFINIDAT.
+### Change Control
+#Version | Who          | What       | When
+#------- | ------------ | ---------- | -------------
+#1.0     | Itai Weisman | Gensis     | July 29th, 2018
+### Usage:
+
+#### For Creating Snapshot
+
+#perl ./snaphandle.pl create <ibox_name> <volume name> <snapshot name>
+
+#### For Mapping Snapshot To Host
+#
+#perl ./snaphandle.pl create <ibox_name> <snapshot name> <host name>
+#
+
+
 use MIME::Base64;
 use Term::ReadKey;
 
